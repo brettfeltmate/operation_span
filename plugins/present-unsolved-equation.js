2d999d21146265e08f09ef16382e7bbbb4829867
brettfelmate <the.bretter.email@gmail.com>
2020-12-06 17:09:09 -0400

    Other than some polishing, present-unsolved-equation.js  complete.

--- plugins/present-unsolved-equation.js
+++ plugins/present-unsolved-equation.js
@@ -31,7 +31,7 @@ jsPsych.plugins["present-unsolved-equation"] = (function() {
             prompt: {
               type: jsPsych.plugins.parameterType.STRING,
               pretty_name: "Prompt message",
-              default: "What is the solution? Press 'continue' when you believe you know.",
+              default: `What is the solution?<br>Press 'continue' when you believe you know.`,
               description: "Any content here will be displayed above the presented expression."
             },
             trial_duration: {
@@ -74,25 +74,115 @@ jsPsych.plugins["present-unsolved-equation"] = (function() {
 
     plugin.trial = function(display_element, trial) {
 
-        plugin.expression = trial.expression;
+        // Event styles
+        $('head').append(
+            $('<style />'). attr('id', 'present-unsolved-equation-styles').html(
+                `body {\n` +
+                `\tbackground-color: rgb(45,45,48);\n` +
+                `\tfont-family: sans-serif;\n` +
+                `\tcolor: white;\n` +
+                `\tfont-size: 20pt;\n`+
+                `}\n\n` +
+                `.wrapper {\n` +
+                `\tposition: fixed;\n` +
+                `\ttop: 50%;\n` +
+                `\tleft: 50%;\n` +
+                `\ttransform: translate(-50%, -50%);\n` +
+                `}\n\n` +
+                `.grid {\n` +
+                `\tdisplay: grid;\n` +
+                `\tgrid-template-columns: 50vw;\n` +
+                `\tgrid-template-rows: 30vh 30vh 15vh 15vh;\n` +
+                `}\n\n` +
+                `.row {\n` +
+                `\tdisplay: flex;\n` +
+                `\tjustify-content: center;\n` +
+                `\talign-items: center;\n` +
+                `\ttext-align: center;\n` +
+                `}\n\n` +
+                `.button {\n` +
+                `\tdisplay: inline-block;\n` +
+                `\tpadding: 10px;\n` +
+                `\ttop: 50%;\n` +
+                `\tleft: 50%;\n` +
+                `\tfont-size: 20pt;\n` +
+                `}`
+            )
+        )
 
-        let full_form = plugin.expression;
+        // Grab expression, and extend if requested
+        let full_form = trial.expression;
 
         if (trial.add_term) {
             full_form = plugin.append_term(full_form)
         }
 
-        plugin.full_form = full_form;
-        plugin.true_answer = math.evaluate(plugin.full_form);
+        // Generate event html
+        var event_html =
+            `<div class = "wrapper">` +
+            `<div class = 'grid'>` +
+            `<div class = "row">${trial.prompt}</div>` +
+            `<div class = 'row' style = 'font-size: 30pt'>${full_form}</div>` +
+            `<div class = 'row'><button class = 'button'>${trial.button_label}</button></div>` +
+            `<div class = 'row'></div>` +
+            `</div></div>`
 
+        display_element.innerHTML = event_html;
 
-        // data saving
-        var trial_data = {
-            parameter_name: 'parameter value'
-        };
+        // Start the clock
+        var start_time = performance.now();
 
-        // end trial
-        jsPsych.finishTrial(trial_data);
+        // add click listener to button
+        $('button').click(function() { after_response(); })
+
+        // store response
+        var response = { rt: 'timeout' };
+
+        // If response made, grab rt
+        function after_response() {
+            // measure rt
+            var end_time = performance.now();
+            var rt = end_time - start_time;
+
+            // disable all the buttons after a response
+            $('button').attr('disabled', 'disabled');
+
+            response.rt = rt;
+
+            if (trial.response_ends_trial) {
+                end_trial();
+            }
+        }
+
+        // end event if provided timelimit is met
+        if (trial.trial_duration !== null) {
+            jsPsych.pluginAPI.setTimeout(function() {
+                end_trial();
+            }, trial.trial_duration);
+        }
+
+        function end_trial() {
+            // ensure button has been disabled
+            $('button').attr('disabled', 'disabled');
+
+            // kill any remaining setTimeout handlers
+            jsPsych.pluginAPI.clearAllTimeouts();
+
+            var trial_data = {
+                "rt": response.rt,
+                "equation": full_form
+            }
+
+            // Remove event styles
+            $('present-unsolved-equation-styles').remove();
+
+            // clear display
+            display_element.innerHTML = "";
+
+            // end & progress to next event
+            jsPsych.finishTrial(trial_data);
+
+        }
     };
 
     return plugin;
